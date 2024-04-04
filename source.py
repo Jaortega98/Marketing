ea32db920a4efcfc52e88c3e258654209904ab49
alejoarango8 <alejandro.arango8@udea.edu.co>
2024-04-03 22:13:38 -0500

    Actualizacion de funciones

--- source.py
+++ source.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import ipywidgets as ipyw       
-from ipywidgets import widgets, interactive_output, interact, interactive, fixed, widget
+from ipywidgets import widgets, interactive_output
 from IPython.display import display
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
@@ -92,34 +92,44 @@ def filter_popularity(type, n, yr, m_ten):
 
         if m_ten:
 
-            query = f"SELECT movieId, title, ROUND(AVG(rating), 3) AS avg_ratings, COUNT(*) AS total_ratings FROM full_info GROUP BY movieId HAVING total_ratings > 9 ORDER BY avg_ratings DESC LIMIT {n}"
+            query = f'SELECT movieId, title AS Titulo, ROUND(AVG(rating), 3) AS "Promedio calificacion", COUNT(*) AS "Veces vista" FROM full_info GROUP BY movieId HAVING "Veces vista" > 9 ORDER BY "Promedio calificacion" DESC LIMIT {n}'
     
             df = create_df(query)
         
-            display(df)
+            display(df.iloc[:, 1:])
         else:
     
-            query = f"SELECT movieId, title, ROUND(AVG(rating), 3) AS avg_ratings, COUNT(*) AS total_ratings FROM full_info GROUP BY movieId ORDER BY avg_ratings DESC LIMIT {n}"
+            query = f'SELECT movieId, title AS Titulo, ROUND(AVG(rating), 3) AS "Promedio calificacion", COUNT(*) AS "Veces vista" FROM full_info GROUP BY movieId ORDER BY "Promedio calificacion" DESC LIMIT {n}'
         
             df = create_df(query)
         
-            display(df)
+            display(df.iloc[:, 1:])
     
     elif type == "Mas Vista":
         
-        query = f"SELECT movieId, title, ROUND(AVG(rating), 3) AS avg_ratings, COUNT(*) AS total_ratings FROM full_info GROUP BY movieId ORDER BY total_ratings DESC LIMIT {n}"
+        query = f'SELECT movieId, title AS Titulo, ROUND(AVG(rating), 3) AS "Promedio calificacion", COUNT(*) AS "Veces vista" FROM full_info GROUP BY movieId ORDER BY "Veces vista" DESC LIMIT {n}'
     
         df = create_df(query)
     
-        display(df)
+        display(df.iloc[:, 1:])
     
     elif type == "Mejor Calificada x Año de lanzamiento":
+
+        if m_ten:
+
+          query = f'SELECT released_yr AS "Año de Lanzamiento", title AS Titulo, ROUND(AVG(rating), 3) AS "Promedio calificacion", COUNT(*) "Veces vista" FROM full_info GROUP BY "Año de Lanzamiento", Titulo HAVING "Año de Lanzamiento" = {yr}  AND "Veces vista" > 9 ORDER BY "Año de Lanzamiento" DESC, "Promedio calificacion" DESC'
+
+          df = create_df(query)
     
-        query = f"SELECT released_yr, title, ROUND(AVG(rating), 3) AS avg_ratings, COUNT(*) total_ratings FROM full_info GROUP BY released_yr, title HAVING released_yr = {yr} ORDER BY released_yr DESC, avg_ratings DESC"
+          display(df.iloc[:n])
+          
+        else:
     
-        df = create_df(query)
-    
-        display(df)
+          query = f'SELECT released_yr AS "Año de Lanzamiento", title AS Titulo, ROUND(AVG(rating), 3) AS "Promedio calificacion", COUNT(*) "Veces vista" FROM full_info GROUP BY "Año de Lanzamiento", Titulo HAVING "Año de Lanzamiento" = {yr} ORDER BY "Año de Lanzamiento" DESC, "Promedio calificacion" DESC'
+      
+          df = create_df(query)
+      
+          display(df.iloc[:n])
 
 
 def popularity_recommendations(max_titles=60):
@@ -128,24 +138,24 @@ def popularity_recommendations(max_titles=60):
     wid_2 = widgets.BoundedIntText(value=10, min=10, max=max_titles)
     wid_3 = widgets.Dropdown(options=create_df("SELECT DISTINCT released_yr FROM full_info").sort_values(by="released_yr").values.flatten().tolist(),
                                disabled=True)
-    wid_4 = widgets.Dropdown(options={"Si":True, "No":False})
+    wid_4 = widgets.Dropdown(options={"Si":True, "No":False}, value=False)
 
     def update_wid3(change):
-        
+
         if change.new == "Mejor Calificada x Año de lanzamiento":
             wid_3.disabled = False
         else:
             wid_3.disabled = True
-            
+
     def update_wid4(change):
-        
-        if change.new == "Mejor Calificada":
+
+        if change.new in ["Mejor Calificada", "Mejor Calificada x Año de lanzamiento"]:
             wid_4.disabled = False
         else:
             wid_4.disabled = True
 
 
-    ui_1 = widgets.VBox([ipyw.HTML("<b>Tipo de filtrado</b>"), wid_1, ipyw.HTML("<b>Cantidad de sugerencias</b>"), 
+    ui_1 = widgets.VBox([ipyw.HTML("<b>Tipo de filtrado</b>"), wid_1, ipyw.HTML("<b>Cantidad de sugerencias</b>"),
                          wid_2, ipyw.HTML("<b>Año de lanzamiento</b>"), wid_3])
 
     ui_2 = widgets.VBox([ipyw.HTML("<b>Vista mas de 10 veces</b>"), wid_4])
@@ -161,17 +171,17 @@ def popularity_recommendations(max_titles=60):
 
 def filter_content(title, n):
     
-    query = "SELECT * FROM movies;"
+    query = "SELECT movieId, title AS Titulo, genres AS Generos FROM movies;"
 
-    movies = create_df(query).set_index("movieId")
+    movies = create_df(query)
 
-    movies["released_yr"] = movies["title"].str.extract("\((\d{4})\)").fillna(2010).astype(int)
+    movies["Año de Lanzamiento"] = movies["Titulo"].str.extract("\((\d{4})\)").fillna(2010).astype(int)
 
     escaler = MinMaxScaler()
 
-    index = movies[movies["title"] ==  title].index.values.tolist()[0]
+    index = movies[movies["Titulo"] ==  title].index.values.tolist()[0]
 
-    to_corr = pd.concat([movies["released_yr"], movies['genres'].str.get_dummies()], axis=1)
+    to_corr = pd.concat([movies["Año de Lanzamiento"], movies['Generos'].str.get_dummies()], axis=1)
 
     to_corr = pd.DataFrame(escaler.fit_transform(to_corr), columns=to_corr.columns, index=to_corr.index)
 
@@ -179,7 +189,7 @@ def filter_content(title, n):
     movies["similitud"] = to_corr.corrwith(to_corr.loc[index], axis=1)
 
 
-    display(movies.sort_values(by="similitud", ascending=False).iloc[:n,:])
+    display(movies[movies["Titulo"] != title].sort_values(by="similitud", ascending=False).iloc[:n,1:])
 
 def content_recommendations(max_titles=60):
 
@@ -196,36 +206,30 @@ def content_recommendations(max_titles=60):
 
 def filter_knn(title, n_neighbors=10):
 
-      query = "SELECT * FROM movies;"
-    
+      query = "SELECT movieId, title AS Titulo, genres AS Generos FROM movies;"
+
       movies = create_df(query)
+
+      movies["Año de Lanzamiento"] = movies["Titulo"].str.extract("\((\d{4})\)").fillna(2010).astype(int)
     
-      movies["released_yr"] = movies["title"].str.extract("\((\d{4})\)").fillna(0).astype(int)
-    
-      to_corr = pd.concat([movies["released_yr"], movies['genres'].str.get_dummies()], axis=1)
+      to_corr = pd.concat([movies["Año de Lanzamiento"], movies['Generos'].str.get_dummies()], axis=1)
     
       model = make_pipeline(MinMaxScaler(),
                             NearestNeighbors(n_neighbors=n_neighbors, metric="cosine"))
     
       model.fit(to_corr)
-    
-    
+
       dis, id_dis = model.named_steps["nearestneighbors"].kneighbors(to_corr)
     
       dis = pd.DataFrame(dis)
     
       id_list = pd.DataFrame(id_dis)
     
-    
-    
-      movies_id = movies[movies['title'] == title].index[0]
-    
-    
+      movies_id = movies[movies['Titulo'] == title].index[0]
     
       inx = id_list.loc[movies_id]
     
-    
-      display(movies.loc[inx,:])
+      display(movies.iloc[inx,1:])
 
 
 def knn_recommendations(max_titles=60):
